81fc32d0f231a91c9d7ca00bf65675f039a8c705
Bruno <sumar.bruno@yahoo.com>
2021-10-10 21:58:20 -0300

    final app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,274 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app" :class="themeName">
+        <div id="main" >
+            <div id="header">
+                <span>calc</span>
+                <div id="themes">
+                    <span>THEME</span>
+                    <div>
+                        <div
+                            v-for="t in [1,2,3]"
+                            :class="{active: t==theme}"
+                            v-on:click="changeTheme(t)"
+                            :key="t"
+                        >
+                            <span >{{t}}</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div id="display"><span>{{ display_val }}</span></div>
+            <div id="buttons">
+                <Button
+                    v-for="btn in btns"
+                    :value="btn"
+                    :key="btn"
+                    :input="input"
+                />
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+ import Button from './components/Button.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+ export default {
+     name: 'App',
+     components: {
+         Button,
+     },
+     data: function() {
+         return {
+             btns: [
+                 7,     8,    9, 'Del',
+                 4,     5,    6,   '+',
+                 1,     2,    3,   '-',
+                 '.',   0,  '/',   'X',
+                 'Reset', '='
+             ],
+             display_val: '',
+             theme: 1,
+         }
+     },
+     computed: {
+         themeName: function () {
+             return ['normal', 'light', 'dark'][this.theme-1];
+         }
+     },
+     methods: {
+         input: function(val) {
+
+             if(this.display_val === 'ERROR')
+                 this.display_val = '';
+
+             switch(val){
+                 case '=':
+                     try{
+                         this.display_val = eval(this.display_val.split('').map(x => x === 'X' ? '*' : x).join('')).toString()
+                     }
+                     catch(e){
+                         console.error(e);
+                         this.display_val = 'ERROR';
+                     }
+                     break;
+                 case 'Reset':
+                     this.display_val = '';
+                     break;
+                 case 'Del':
+                     this.display_val = this.display_val.slice(0, -1);
+                     break;
+                 default:
+                     this.display_val = this.display_val.concat(val)
+             }
+         },
+         changeTheme: function(val){
+             this.theme = val;
+         }
+     }
+ }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+ body {
+     margin: 0;
+     border: 0;
+     padding: 0;
+
+     width: 100vw;
+     height: 100vh;
+ }
+
+ #app{
+     background: #3b4664;
+     display: flex;
+     justify-content: center;
+
+     width: 100%;
+     height: 100%;
+
+     -webkit-font-smoothing: antialiased;
+     -moz-osx-font-smoothing: grayscale;
+     text-align: center;
+     color: #2c3e50;
+ }
+
+
+
+ #main {
+     width: 90%;
+     height: 100%;
+     min-width: 375px;
+     max-width: 600px;
+
+     display: flex;
+     flex-direction: column;
+     justify-content: center;
+     color: white;
+
+     font-family: 'Spartan';
+     font-weight: 700;
+ }
+
+ #main > div {
+     margin: .75rem 0;
+ }
+
+ #header {
+     font-size: 2.1rem;
+     display: flex;
+     flex-direction: row;
+     justify-content: space-between;
+     /* align-items: end; */
+     line-height: 1.7rem;
+ }
+
+ #themes {
+     font-size: .8rem;
+     display: flex;
+     flex-direction: row;
+     justify-content: space-between;
+     /* font-weight: 600; */
+
+     padding: .3rem;
+ }
+
+ #themes > div {
+     background: #252c46;
+     border-radius: 1rem;
+     display: flex;
+     flex-direction: row;
+     justify-content: space-between;
+     align-items: center;
+     width: 3.9rem;
+     padding: 0 .4rem;
+     margin-left: 1rem;
+     /* overflow: hidden; */
+ }
+
+ #themes > div > div {
+     font-size: .8rem;
+     border-radius: 2rem;
+     width: 1rem;
+     height: 1rem;
+     display: inline-block;
+     /* overflow: hidden; */
+ }
+
+ #themes > div > div > span {
+     display: inline-block;
+     transform: translateY(-2rem);
+     height: 3rem;
+     width: 1.1rem;
+     cursor: pointer;
+ }
+
+ .active {
+     background: #d53d32;
+ }
+
+ #display {
+     background: hsl(224, 36%, 15%);
+     border-radius: .8rem;
+     font-size: 4rem;
+     padding: 5%;
+     text-align: right;
+     direction: rtl;
+     text-overflow: ellipsis;
+     overflow-x: hidden;
+     white-space: nowrap;
+     line-height: 5rem;
+     min-height: 5rem;
+
+ }
+
+ #display > span {
+     direction: ltr;
+     display: inline-block;
+ }
+
+ #buttons {
+     padding: 5%;
+     height: 55%;
+     display: grid;
+     grid-template-columns: auto auto auto auto;
+     grid-gap: 5.8%;
+
+     background: #252d44;
+     border-radius: .8rem;
+ }
+
+ @font-face {
+     font-family: 'Spartan';
+     src: url('/Spartan-VariableFont_wght.ttf');
+ }
+
+ /* Themes */
+
+ /* Light */
+ div.light {
+     background: #e6e6e6 !important;
+ }
+
+ .light div {
+     color: #353529 !important;
+ }
+
+ .light #buttons {
+     background: #d3cdcd;
+ }
+
+ .light #display {
+     background: #eeeeee;
+ }
+
+ .light #themes > div {
+     background: #d3cdcd;
+ }
+
+ /* Dark */
+ div.dark {
+     background: #17062a !important;
+ }
+
+ .dark div {
+     color: #ffe13e !important;
+ }
+
+ .dark #buttons {
+     background: #1e0837;
+ }
+
+ .dark #display {
+     background: #1e0836;
+ }
+
+ .dark #themes > div {
+     background: #1e0837;
+ }
+
+ .dark div.active {
+     background: #00e4d8 !important;
+ }
+
 </style>
---
+++ src/components/Button.vue
@@ -0,0 +1,131 @@
+<template class="button">
+    <input
+        type="button"
+        :value="value"
+        :class="btn_type"
+        v-on:click="input(value)"
+    >
+</template>
+
+<script>
+
+ export default {
+     name: 'App',
+     components: {
+     },
+     props: [ 'value', 'input'],
+     computed: {
+         btn_type: function() {
+             switch(this.value){
+                 case 0:
+                 case 1:
+                 case 2:
+                 case 3:
+                 case 4:
+                 case 5:
+                 case 6:
+                 case 7:
+                 case 8:
+                 case 9:
+                     return 'number';
+                 case '=':
+                     return 'equal';
+                 case '+':
+                     return 'plux';
+                 case '-':
+                     return 'minus';
+                 case '.':
+                     return 'dot';
+                 case '/':
+                     return 'div';
+                 default:
+                     return this.value;
+             }
+         },
+     }
+
+ }
+</script>
+
+<style scoped>
+ input {
+     font-size: 2.2rem;
+     font-weight: 600;
+     color: #424b5a;
+     cursor: pointer;
+
+     border: 0;
+     border-radius: .7rem;
+     box-shadow: 0 .3rem  #b2a295;
+     /* box-sizing: border-box; */
+ }
+
+ input:active {
+     box-shadow: unset;
+     transform: translateY(.35rem)
+ }
+
+ .Reset {
+     background-color: #647299;
+     box-shadow: 0 .3rem  #424e74;
+     color: white;
+
+     grid-column-start: 1;
+     grid-column-end: 3;
+
+ }
+
+ .Del {
+     background-color: #647299;
+     box-shadow: 0 .3rem  #424e74;
+     color: white;
+ }
+
+
+ .equal {
+     background-color: #d13f30;
+     box-shadow: 0 .3rem  #8f2316;
+     color: white;
+
+     grid-column-start: 3;
+     grid-column-end: 5;
+ }
+
+ /* Themes */
+
+ /* Light */
+ .light input {
+     background: #e5e4e0;
+     box-shadow: 0 .3rem #a79d91;
+ }
+
+ .light .equal {
+     background: #c85401;
+     box-shadow: 0 .3rem #893a02;
+ }
+
+ .light .Reset, .light .Del {
+     background: #388187;
+     box-shadow: 0 .3rem #1c6168;
+ }
+
+ /* Dark */
+ .dark input {
+     background: #331b4d;
+     color: #ffe846;
+     box-shadow: 0 .3rem #851c9d;
+ }
+
+ .dark .equal {
+     background: #00decf;
+     box-shadow: 0 .3rem #6df9f0;
+     color: #002830;
+ }
+
+ .dark .Reset, .dark .Del {
+     background: #56077c;
+     box-shadow: 0 .3rem #bf16f5;
+     color: #fefff8;
+ }
+
+</style>
